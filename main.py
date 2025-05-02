8c54ee05d2138e390d2b0a53c4c29bbf21c442d4
Nkwor-Jane <nkwor.jane.chinelo@gmail.com>
2025-05-02 17:05:14 +0100

    ALT/SOE/024/2727 added unlike functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, HTTPException, status
 from pydantic import BaseModel, EmailStr
 from typing import Annotated
+
 
 my_app = FastAPI()
 
@@ -17,9 +18,28 @@ new_user_db: dict[str, UserCreate] = {}
 def home():
     return {"message": "Welcome to Mini Social Media API!"}
 
+
 @my_app.post("/login/")
 async def returning_user_login(
     username: Annotated[str, Form()],
     password: Annotated[str, Form()],
 ):
     return {"message": f"Hi {username}, welcome back!"}
+
+# To unlike a post
+@my_app.post("/posts/{post_id}/unlike/")
+def unlike_post(post_id: int, username:str):
+    for post in posts:
+        if post["id"] == post_id:
+            if (username, post_id) not in likes:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail = "You have not liked this post yet"
+                )
+            likes.remove((username, post_id))
+            post["likes"] = max(0, post["likes"] - 1)
+            return {"Message": "Post unliked!", "likes": post["likes"]}
+    raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail = "Post not found"
+                )
