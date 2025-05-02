86fc5bb4c4fcf6dc44bb625ad2f412f3d6802f9e
Mildred Obiora <obioramildred@gmail.com>
2025-05-02 11:43:11 +0100

    ALT/SOE/024/5545 User-related routes1

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Form
+from pydantic import BaseModel, EmailStr
+from typing import Annotated
+
+my_app = FastAPI()
+
+# Pydantic model for user creation
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str
+
+# Dictionary to simulate storing users (in-memory)
+new_user_db: dict[str, UserCreate] = {}
+
+@my_app.get("/")
+def home():
+    return {"message": "Welcome to Mini Social Media API!"}
+
+@my_app.post("/login/")
+async def returning_user_login(
+    username: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+):
+    return {"message": f"Hi {username}, welcome back!"}
