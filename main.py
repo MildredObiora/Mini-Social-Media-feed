2288e43034df9db42fb83800c55ed77b1d2d6238
Adeyemi Adeniyi <adeyemiadeniyi97@gmail.com>
2025-05-02 12:33:09 +0100

    fix: Added users reg route and list users route

--- main.py
+++ main.py
@@ -23,3 +23,15 @@ async def returning_user_login(
     password: Annotated[str, Form()],
 ):
     return {"message": f"Hi {username}, welcome back!"}
+
+@my_app.post("/users/")
+async def register_user(user: UserCreate):
+    if user.username in new_user_db:
+        return {"error": "Username already exists"}
+    
+    new_user_db[user.username] = user
+    return {"message": "User registered successfully", "user": user}
+
+@my_app.get("/users/")
+async def list_users():
+    return {"users": list(new_user_db.values())}
